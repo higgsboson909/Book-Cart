0e8405c994a5f950f38d4a34037ebecec0a63d02
higgsboson909 <higgsbosson906@gmail.com>
2025-09-05 00:21:13 +0500

    Fetch books data

---
+++ src/utils/getData.js
@@ -0,0 +1,23 @@
+const getData = async () => {
+  try {
+    const response = await fetch(
+      "https://openlibrary.org/people/mekBot/books/currently-reading.json",
+      { mode: "cors" },
+    );
+    if (response.status >= 400) {
+      throw new Error("Network Error");
+    }
+
+    const booksData = await response.json();
+    return booksData.reading_log_entries.map((item, index) => ({
+      id: index,
+      title: item.work.title,
+      cover: `https://covers.openlibrary.org/b/id/${item.work.cover_id}-L.jpg`,
+    }));
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export { getData };
