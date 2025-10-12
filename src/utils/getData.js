d8c2f1ff08fd5df32df8901d56dcbf8c6a3f7357
higgsboson909 <higgsbosson906@gmail.com>
2025-10-12 10:47:47 +0500

    Add hd covers and set those

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,3 +1,47 @@
+import cover1 from "../assets/covers/1.jpeg";
+import cover2 from "../assets/covers/2.jpeg";
+import cover3 from "../assets/covers/3.jpeg";
+import cover4 from "../assets/covers/4.jpeg";
+import cover5 from "../assets/covers/5.jpeg";
+import cover6 from "../assets/covers/6.jpg";
+import cover7 from "../assets/covers/7.png";
+import cover8 from "../assets/covers/8.jpeg";
+import cover9 from "../assets/covers/9.jpg";
+import cover10 from "../assets/covers/10.jpg";
+import cover11 from "../assets/covers/11.jpeg";
+import cover12 from "../assets/covers/12.jpg";
+import cover13 from "../assets/covers/13.jpg";
+import cover14 from "../assets/covers/14.jpg";
+import cover15 from "../assets/covers/15.jpg";
+import cover16 from "../assets/covers/16.jpg";
+import cover17 from "../assets/covers/17.jpg";
+import cover18 from "../assets/covers/18.jpg";
+import cover19 from "../assets/covers/19.jpg";
+import cover20 from "../assets/covers/20.jpg";
+
+const covers = [
+  cover1,
+  cover2,
+  cover3,
+  cover4,
+  cover5,
+  cover6,
+  cover7,
+  cover8,
+  cover9,
+  cover10,
+  cover11,
+  cover12,
+  cover13,
+  cover14,
+  cover15,
+  cover16,
+  cover17,
+  cover18,
+  cover19,
+  cover20,
+];
+
 import { price } from "./price";
 const getData = async () => {
   try {
@@ -13,7 +57,7 @@ const getData = async () => {
     return booksData.reading_log_entries.map((item, index) => ({
       id: index,
       title: item.work.title,
-      cover: `https://covers.openlibrary.org/b/id/${item.work.cover_id}-L.jpg`,
+      cover: covers[index],
       authors: item.work.author_names,
       price: price[index],
     }));
